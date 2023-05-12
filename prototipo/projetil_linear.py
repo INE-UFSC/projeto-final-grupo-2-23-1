49e86688e7b02c3c8cc823034b47e97e850fd260
Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
2023-05-12 11:09:20 -0300

    Adicionar movimentação do projétil linear

--- prototipo/projetil_linear.py
+++ prototipo/projetil_linear.py
@@ -1,18 +1,27 @@
+import pygame as pg
 from projetil import Projetil
+from math import cos, sin
 
-class ProjetilLinear(Projetil):
-    def __init__(self, dano: int, veloc_proj: float, tamanho: float, perfuracao: int):
-        super.__init__(dano = dano, veloc_proj = veloc_proj, tamanho = tamanho, perfuracao = perfuracao)
-        self.__dano = dano
-        self.__veloc_proj = veloc_proj
-        self.__tamanho = tamanho
-        self.__perfuracao = perfuracao
+class ProjetilLinear(Projetil, pg.sprite.Sprite):
+    def __init__(self, dano: int, veloc_proj: float, tamanho: float, perfuracao: int, angulo: float):
+        Projetil.__init__(self, dano, veloc_proj, tamanho, perfuracao)
+        pg.sprite.Sprite.__init__(self)
 
-    def atualizar_trajetoria(self):
-        pass
+        self.angulo = angulo
 
+        self.image = pg.Surface((10, 10))
+        pg.draw.circle(self.image, 'white', (5,5), 5)
+        self.rect = self.image.get_rect(center = (200, 200))
+        self.pos = pg.math.Vector2(self.rect.center)
+
+    def atualizar_trajetoria(self, dt):
+        self.pos.x += cos(self.angulo)*self.veloc_proj*dt
+        self.pos.y -= sin(self.angulo)*self.veloc_proj*dt
+
+        self.rect.center = self.pos        
+
+    def update(self, dt):
+        self.atualizar_trajetoria(dt)
 
     def sofrer_impacto(self):
-        pass
-
-        
+        self.kill()
--- prototipo/projetilteleguiado.py
+++ prototipo/projetilteleguiado.py
@@ -2,11 +2,7 @@ from projetil import Projetil
 
 class ProjetilTeleguiado(Projetil):
     def __init__(self, dano: int, veloc_proj: float, tamanho: float, perfuracao: int):
-        super().__init__(dano = dano, veloc_proj = veloc_proj, tamanho = tamanho, perfuracao = perfuracao)
-        self.__dano = dano
-        self.__veloc_proj = veloc_proj
-        self.__tamanho = tamanho
-        self.__perfuracao = perfuracao
+        super().__init__(dano, veloc_proj, tamanho, perfuracao)
 
     def atualizar_trajetoria(self):
         pass

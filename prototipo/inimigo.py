5b243fbfee49cdf7d6c8a789aaa24acc7069b779
Thiago Kenzo Takahashi <thiagokenzo7@gmail.com>
2023-05-12 09:34:08 -0300

    arrumei inimigo

--- prototipo/inimigo.py
+++ prototipo/inimigo.py
@@ -2,26 +2,7 @@ from entidade import *
 
 class Inimigo(Entidade):
     def __init__(self, vida_inicial, veloc_mov, veloc_tiro):
-        super().__init__(vida_inicial = vida_inicial, veloc_mov = veloc_mov, veloc_tiro = veloc_tiro)
-        self.__vida_inicial = vida_inicial
-        self.__veloc_mov = veloc_mov
-        self.__veloc_tiro = veloc_tiro
-
-    @property
-    def vida_inicial(self):
-        return self.__vida_inicial
-    
-    @property
-    def veloc_mov(self):
-        return self.__veloc_mov
-    
-    @property
-    def veloc_tiro(self):
-        return self.__veloc_tiro
-    
-    @abstractmethod
-    def sofrer_dano(self, dano: int):
-        self.__dano = self.__dano - dano
+        super().__init__(vida_inicial, veloc_mov, veloc_tiro)
 
     @abstractmethod
     def mover(self):
@@ -30,7 +11,4 @@ class Inimigo(Entidade):
     @abstractmethod
     def atirar(self):
         pass 
-
-    
-
     

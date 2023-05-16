3c24d4375b796ade67fce01bd5e787bc034d79bf
Pedro Alfeu Wolff Lemos <lucbsm@gmail.com>
2023-05-16 11:12:15 -0300

    Orientando mapa a objetos

--- prototipo/mapa.py
+++ prototipo/mapa.py
@@ -6,6 +6,14 @@ COLOR = (255, 100, 98)
 SURFACE_COLOR = (20, 23, 26)
 WIDTH = 0
 HEIGHT = 0
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+size = (WIDTH, HEIGHT)
+screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+pygame.display.set_caption("Creating Sprite")
+all_sprites_list = pygame.sprite.Group()
+exit = True
+clock = pygame.time.Clock()
   
 # Object class
 class Sprite(pygame.sprite.Sprite):
@@ -19,80 +27,49 @@ class Sprite(pygame.sprite.Sprite):
         pygame.draw.rect(self.image,color,pygame.Rect(0, 0, width, height))
   
         self.rect = self.image.get_rect()
+
+class Draw:
+    def draw():
+        objects = []
+
+        objects.append(Sprite(RED, 900, 30))
+        objects[len(objects)-1].rect.x = 0
+        objects[len(objects)-1].rect.y = 0
+
+        objects.append(Sprite(WHITE, 1000, 3000))
+        objects[len(objects)-1].rect.x = 0
+        objects[len(objects)-1].rect.y = 900
+
+        objects.append(Sprite(RED, 900, 30))
+        objects[len(objects)-1].rect.x = 1890
+        objects[len(objects)-1].rect.y = 0
+
+        objects.append(Sprite(WHITE, 550, 200))
+        objects[len(objects)-1].rect.x = 30
+        objects[len(objects)-1].rect.y = 700
+
+        objects.append(Sprite(WHITE, 550, 300))
+        objects[len(objects)-1].rect.x = 230
+        objects[len(objects)-1].rect.y = 800
+
+        objects.append(Sprite(WHITE, 550, 890))
+        objects[len(objects)-1].rect.x = 1000
+        objects[len(objects)-1].rect.y = 800
+
+        objects.append(Sprite(WHITE, 550, 490))
+        objects[len(objects)-1].rect.x = 1400
+        objects[len(objects)-1].rect.y = 700
+
+        return objects
   
-  
-pygame.init()
-  
-RED = (255, 0, 0)
-WHITE = (255, 255, 255)
 
-  
-size = (WIDTH, HEIGHT)
-screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
-pygame.display.set_caption("Creating Sprite")
-  
-all_sprites_list = pygame.sprite.Group()
-
-
-
-floor = (Sprite(WHITE, 1000, 3000))
-floor.rect.x = 0
-floor.rect.y = 900
-
-
-limite_esquerda = (Sprite(RED, 900, 30))
-limite_esquerda.rect.x = 0
-limite_esquerda.rect.y = 0
-
-limite_direita = (Sprite(RED, 900, 30))
-limite_direita.rect.x = 1890
-limite_direita.rect.y = 0
-
-
-"""  
-floor = []
-x = 1920
-y = 900
-for i in range(3):
-    floor.append(Sprite(WHITE, 1000, 100))
-    floor[i].rect.x = x
-    floor[i].rect.y = y
-    x += 100
-    y -= 100
-
-
-x1 = 900
-y1 = 900
-for i in range(3):
-    floor.append(Sprite(WHITE, 1000, 100))
-    floor[i].rect.x = x1
-    floor[i].rect.y = y1
-    x1 += 100
-    y1 -= 100
-
-
-
-floor.append(Sprite(WHITE, 1000, 3000))
-floor[len(floor)-1].rect.x = 0
-floor[len(floor)-1].rect.y = 900
-
-
-
-for i in range(len(floor)):  
-    all_sprites_list.add(floor[i])
-"""  
-
-all_sprites_list.add(floor)
-all_sprites_list.add(limite_esquerda)
-all_sprites_list.add(limite_direita)
-exit = True
-clock = pygame.time.Clock()
-  
+pygame.init() 
 while exit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = False
-        
+    
+    all_sprites_list.add(Draw.draw())
     all_sprites_list.update()
     screen.fill(SURFACE_COLOR)
     all_sprites_list.draw(screen)

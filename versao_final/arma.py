6b1a7fc3404a453378a42623ffd48cbbd4dc23fe
alfeu23 <127217741+alfeu23@users.noreply.github.com>
2023-06-23 10:32:01 -0300

    Adicionando pistola

--- versao_final/arma.py
+++ versao_final/arma.py
@@ -7,9 +7,9 @@ from acessorio import Acessorio
 from projetil import Projetil
 
 
-class Arma(pg.sprite.Sprite):
-    def __init__(self, nome, img_arquivo, tempo_recarga, tipo_projetil: Projetil):
-        #Acessorio.__init__(self, nome, custo, descricao)
+class Arma(Acessorio, pg.sprite.Sprite):
+    def __init__(self, nome, custo, descricao, tipo_projetil: Projetil):
+        Acessorio.__init__(self, nome, custo, descricao)
         pg.sprite.Sprite.__init__(self)
 
         self.__tipo_projetil = tipo_projetil
@@ -17,8 +17,8 @@ class Arma(pg.sprite.Sprite):
         self.__mira_angulo = 0
 
         # TODO: adicionar mais armas e deixar o sprite customizável.
-        arma_img = pg.image.load(os.path.join('sprites', 'armas', img_arquivo)).convert_alpha()
-        self.image = pg.transform.scale(arma_img, (70, 18))
+        arma_img = pg.image.load(os.path.join('sprites', 'pistola_sprite.png')).convert_alpha()
+        self.image = pg.transform.scale(arma_img, (50, 28))
         self.rect = self.image.get_rect()
         # Esse vetor determina em qual posição o projétil é lançado.
         self.tiro_pos = pg.math.Vector2(self.rect.topright)
@@ -26,12 +26,8 @@ class Arma(pg.sprite.Sprite):
         self.imagem_original = self.image
 
         # TODO: colocar tempo de recarga como um parâmetro.
-        self.TEMPO_RECARGA = tempo_recarga
+        self.TEMPO_RECARGA = 0.75
         self.tiro_temporizador = self.TEMPO_RECARGA/2
-
-    @property
-    def tipo_projetil(self):
-        return self.__tipo_projetil
 
     def atirar_projetil(self):
         if self.tiro_temporizador < self.TEMPO_RECARGA:
@@ -43,8 +39,7 @@ class Arma(pg.sprite.Sprite):
             self.tiro_pos,
             self.__mira_angulo
         )
-        tiro_som = mixer.Sound(os.path.join('musica', 'som_pistola_longa.wav'))
-        tiro_som.play()
+        
         return proj
 
     def rotacionar(self, angulo, pivo):
